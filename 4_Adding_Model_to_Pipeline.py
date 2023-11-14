3c7a6e90ee4ca2aa9d98ef3471481ae1b878a4b2
SaifurR <90628681+SaifurRR@users.noreply.github.com>
2023-11-13 22:53:37 -0800

    Update 4_Adding_Model_to_Pipeline.py

--- 4_Adding_Model_to_Pipeline.py
+++ 4_Adding_Model_to_Pipeline.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn import svm, datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
@@ -14,10 +13,12 @@ from sklearn.metrics import r2_score
 columns = ["sex","length","diam","height","whole","shucked","viscera","shell","age"]
 df = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data",names=columns)
 
-y = df.age
-X=df.drop(columns=['age'])
+y = df.age #target
+X=df.drop(columns=['age']) #features
+
 num_cols = X.select_dtypes(include=np.number).columns
 cat_cols = X.select_dtypes(include=['object']).columns
+
 #create some missing values
 for i in range(1000):
     X.loc[np.random.choice(X.index),np.random.choice(X.columns)] = np.nan
@@ -34,10 +35,12 @@ preprocess = ColumnTransformer(
         ("num_process", num_vals, num_cols)
     ]
 )
-#1. Create a pipeline with `preprocess` and a linear regression model, `regr`
-pipeline=Pipeline([("preprocess", preprocess),("regr ", LinearRegression())]) # cat: simple_impute, ohe, regr ; num: simple_impute, standardscale, regr
 
-#2. Fit the pipeline on the training data and predict on the test data
+#1. Create a pipeline with `preprocess` and a linear regression model, `regr`
+pipeline=Pipeline([("preprocess", preprocess),("regr ", LinearRegression())]) # cat: simple_imputer, ohe, regr ; num: simple_imputer, standardscaler, regr
+
+#2. Fit the pipeline on the training data and predict on test data
+#fit train data
 pipeline.fit(x_train, y_train)
 #prediction on test data
 y_pred=pipeline.predict(x_test)
